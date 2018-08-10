4cdbf6eeb1ad5e50b1b7645f5c0b4982636c2eda
Sean Swanson <sean-swan@hotmail.com>
2018-08-10 08:38:27 -0700

    Added ability to change difficulty, affecting the number of tiles to play with.

--- js/main.js
+++ js/main.js
@@ -1,49 +1,96 @@
 (function() {
   // ----------
-  var colorSquares = document.querySelectorAll(".color-square-tile");
-  colorSquares = Array.prototype.slice.call(colorSquares);
+  var colorSquareEl;
+  var colorSquareEls;
 
   var colors = [];
+  var numberOfColors;
 
   var targetColorHeading = document.querySelector(".rgb-to-guess");
   var targetColor;
 
-  var menuTop = document.querySelector('.menu-top');
-  var menuMessage = document.querySelector('.menu-message');
-  var gameArea = document.querySelector('.game-area');
+  var menuTop = document.querySelector(".menu-top");
+  var menuMessage = document.querySelector(".menu-message");
+  var newGameButton = document.querySelector(".new-game");
+  var easyButton = document.querySelector(".easy-button");
+  var hardButton = document.querySelector(".hard-button");
+  var gameArea = document.querySelector(".game-area");
+  var colorSquareContainer = document.querySelector(".color-square-container");
 
-  var App = {
+  var score = 0;
+  var difficulty = "easy";
+
+  window.App = {
     initGame: function() {
-      this.makeRGBString();
+      colors = [];
+      console.log(colors);
+      this.setDifficulty(difficulty);
+      this.makeRGBStrings();
+      this.makeSquares();
+      this.setSquareColors();
+      this.setTargetColor();
       this.setHandlers();
     },
 
-    setSquareColors: function(rgbString) {
-      var self = this;
-      colorSquares.forEach(function(colorSquare, i) {
-        colorSquare.style.backgroundColor = colors[i];
-      });
+    // ----------
+    makeSquares: function() {
+      while (colorSquareContainer.firstChild) {
+        colorSquareContainer.removeChild(colorSquareContainer.firstChild);
+      }
+      for (var i = 0; i <= colors.length - 1; i++) {
+        console.log(colors.length);
+        colorSquareEl = document.createElement("div");
+        colorSquareEl.classList = "color-square-tile";
+        colorSquareContainer.appendChild(colorSquareEl);
+      }
+      colorSquareEls = document.querySelectorAll(".color-square-tile");
+      colorSquareEls = Array.prototype.slice.call(colorSquareEls);
+      console.log(colorSquareEls);
     },
 
-    makeRGBString: function(num) {
-      var r, g, b, rgbString;
-      for (var i = 0; i <= colorSquares.length - 1; i++) {
-        r = this.getRandomNumber();
-        g = this.getRandomNumber();
-        b = this.getRandomNumber();
-        rgbString = "rgb(" + r + ", " + g + ", " + b + ")";
-        colors.push(rgbString);
-        this.setSquareColors(rgbString);
-        targetColor =
-          colors[Math.floor(Math.random() * (colorSquares.length - 1))];
-        targetColorHeading.textContent = this.capitalizeRGBString();
+    // ----------
+    setDifficulty: function(difficulty) {
+      if (difficulty === "easy") {
+        numberOfColors = 2;
+      } else if (difficulty === "hard") {
+        numberOfColors = 5;
       }
     },
 
-    getRandomNumber: function() {
-      return Math.floor(Math.random() * 255);
+    // ----------
+    setSquareColors: function() {
+      var self = this;
+      colorSquareEls.forEach(function(colorSquare, i) {
+        colorSquare.style.backgroundColor = colors[i];
+        colorSquare.style.pointerEvents = "initial";
+      });
     },
 
+    // ----------
+    makeRGBStrings: function() {
+      var r, g, b, rgbString;
+      for (var i = 0; i <= numberOfColors; i++) {
+        console.log(i);
+        r = this.getRandomColor();
+        g = this.getRandomColor();
+        b = this.getRandomColor();
+        rgbString = "rgb(" + r + ", " + g + ", " + b + ")";
+        colors.push(rgbString);
+      }
+    },
+
+    // ----------
+    setTargetColor: function() {
+      targetColor = colors[Math.floor(Math.random() * colors.length)];
+      targetColorHeading.textContent = this.capitalizeRGBString();
+    },
+
+    // ----------
+    getRandomColor: function() {
+      return Math.floor(Math.random() * 256);
+    },
+
+    // ----------
     capitalizeRGBString: function() {
       var cleanTargetColorRGB =
         (targetColor ? targetColor.slice(0, 3).toUpperCase() : "failed") +
@@ -51,41 +98,67 @@
       return cleanTargetColorRGB;
     },
 
+    // ----------
     checkMatch: function() {
       if (this.style.backgroundColor === targetColor) {
         menuTop.style.backgroundColor = this.style.backgroundColor;
-        menuMessage.textContent = 'Correct!';
-        this.style.pointerEvents = 'none';
+        menuMessage.textContent = "Correct!";
+        newGameButton.textContent = "Play again?";
+        this.style.pointerEvents = "none";
+        score++;
         App.winningTileAnimation();
       } else {
-        this.style.backgroundColor = '#232323';
-        this.style.pointerEvents = 'none';
-        menuMessage.textContent = 'Try again!';
+        this.style.backgroundColor = "#232323";
+        this.style.pointerEvents = "none";
+        score--;
+        menuMessage.textContent = "Try again!";
       }
       console.log(this.style.backgroundColor);
     },
 
+    // ----------
     winningTileAnimation: function() {
       var i = 0;
       (function delayedAnimation() {
-        colorSquares[i].style.backgroundColor = targetColor;
+        colorSquareEls[i].style.backgroundColor = targetColor;
+        colorSquareEls[i].style.border = "2px solid white";
         i++;
-        if (i < colorSquares.length) {
+        if (i < colorSquareEls.length) {
           setTimeout(delayedAnimation, 130);
         }
       })();
     },
 
+    // ----------
     setHandlers: function() {
-      for (var i = 0; i <= colorSquares.length - 1; i++) {
-        colorSquares[i].addEventListener('mouseover', function() {
-          this.classList.add('tile-hover');
+      var self = this;
+      for (var i = 0; i <= colorSquareEls.length - 1; i++) {
+        colorSquareEls[i].addEventListener("mouseover", function() {
+          this.classList.add("tile-hover");
         });
-        colorSquares[i].addEventListener('mouseout', function() {
-          this.classList.remove('tile-hover');
+        colorSquareEls[i].addEventListener("mouseout", function() {
+          this.classList.remove("tile-hover");
         });
-        colorSquares[i].addEventListener('click', this.checkMatch);
+        colorSquareEls[i].addEventListener("click", this.checkMatch);
       }
+      easyButton.addEventListener('click', function() {
+        difficulty = 'easy';
+        easyButton.classList.add('selected-menu-button');
+        hardButton.classList.remove('selected-menu-button');
+        self.initGame();
+      });
+      hardButton.addEventListener('click', function() {
+        difficulty = 'hard';
+        easyButton.classList.remove('selected-menu-button');
+        hardButton.classList.add('selected-menu-button');
+        self.initGame();
+      });
+      newGameButton.addEventListener("click", function() {
+        menuTop.style.backgroundColor = "rgb(83,132,179)";
+        menuMessage.textContent = "";
+        newGameButton.textContent = 'NEW COLORS';
+        self.initGame();
+      });
     }
   };
 
